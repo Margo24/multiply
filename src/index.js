5dddd0c398dd6fcb725c58afbc3e1d7ff22304de
‘Margo24’ <margonessa24@gmail.com>
2019-10-11 08:12:06 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -3,28 +3,13 @@ module.exports = function multiply(first, second) {
     if (first === '0' || second === '0') {
         return '0'
     } else {
-        const a = first.split('');//.reverse();
-        const b = second.split('');//.reverse();
-        let result = [];
-        let res = 0;
-        for (let x = 0; x < b.length; x++) {
-            res = 0;
-            for (i = 0; i < a.length; i++) {
-                let m = a[i] * b[x];
-                res = res + m*Math.pow(10,a.length-1-i);
-                //console.log(m);
-            }
-            result.push(res);
+        const first_num = BigInt(first);
+        const second_num = BigInt(second);
+        const mult = BigInt(first_num*second_num);
+        // convert number to string
+        const result = mult.toString();
+        return result;
         }
-        let tran = result[result.length - 1];
-        let outString = '';
-        for (i=result.length-1; i>0; i--){
-            tran = result[i-1]*10+tran;
-            outString=tran%10+outString;
-            tran = Math.floor(tran/10);
-        }
-        return tran+outString;
-    }
 }
 
 //console.log(multiply('329568934658432659586', '58379426534596'));
